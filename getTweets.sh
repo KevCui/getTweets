f7ded5a53e9038e59a5b15f026a5fe5032f122fa
Kevin Cui <krazycavin@gmail.com>
2020-03-12 08:54:17 +0100

    Filter out user's tweets only

--- getTweets.sh
+++ getTweets.sh
@@ -78,7 +78,8 @@ fetch_tweets() {
     $_CURL -sS "$_API_URL/2/timeline/profile/$1.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_composer_source=true&include_ext_alt_text=true&include_reply_count=1&tweet_mode=extended&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&send_error_codes=true&simple_quoted_tweets=true&include_tweet_replies=true&count=$_MAX_TWEETS&ext=mediaStats%2ChighlightedLabel%2CcameraMoment" \
         --header "authorization: Bearer $2" \
         --header "x-guest-token: $3" \
-        | $_JQ -r '.globalObjects.tweets'
+        | $_JQ '.globalObjects.tweets | .[] | select(.user_id_str==$id)' --arg id "$1" \
+        | $_JQ -s 'sort_by(.id_str)'
 }
 
 main() {
